ae232b1a5bc9748b0d518b8b1175720299f20a1a
Wachtel <mitchel.wachtel@gmail.com>
2021-08-22 20:24:46 -0400

    First commit, again

---
+++ MixedMessages.js
@@ -0,0 +1,12 @@
+console.log('This is a test for mixed messages');
+
+const ran1 = Math.floor(Math.random()*5);
+const ran2 = Math.floor(Math.random()*5);
+const ran3 = Math.floor(Math.random()*5);
+
+const array1 = ['The world is ', 'The day is ', 'The party is ', 'The house is ', 'The game is '];
+const array2 = ['flat ', 'booty ', 'smelly ', 'sad ', 'out of control '];
+const array3 = ['so deal with it!', 'because the music is too loud.', 'therefore it is time to go to sleep.', '- can you believe it?', 'and that is why we cannot have nice things.'];
+
+console.log(array1[ran1] + array2[ran2] + array3[ran3]);
+
