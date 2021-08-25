1f385eef4d5451397cdaaedcb195066f82038b10
Wachtel <mitchel.wachtel@gmail.com>
2021-08-24 20:01:52 -0400

    Made Object to house arrays

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,12 +1,13 @@
-console.log('This is a test for mixed messages');
 
 const ran1 = Math.floor(Math.random()*5);
 const ran2 = Math.floor(Math.random()*5);
-const ran3 = Math.floor(Math.random()*5);
+const ran3 = Math.floor(Math.random()*6);
 
-const array1 = ['The world is ', 'The day is ', 'The party is ', 'The house is ', 'The game is '];
-const array2 = ['flat ', 'booty ', 'smelly ', 'sad ', 'out of control '];
-const array3 = ['so deal with it!', 'because the music is too loud.', 'therefore it is time to go to sleep.', '- can you believe it?', 'and that is why we cannot have nice things.'];
+let ObjectOfArrays = {
+  array1: ['The world is ', 'The day is ', 'The party is ', 'The house is ', 'The game is '],
+  array2: ['flat ', 'booty ', 'smelly ', 'sad ', 'out of control '],
+  array3: ['so deal with it!', 'because the music is too loud.', 'therefore it is time to go to sleep.', '- can you believe it?', 'and that is why we cannot have nice things.', 'and that is the way the cookie crumbles']
 
-console.log(array1[ran1] + array2[ran2] + array3[ran3]);
+}
+console.log(ObjectOfArrays.array1[ran1] + ObjectOfArrays.array2[ran2] + ObjectOfArrays.array3[ran3]);
 
